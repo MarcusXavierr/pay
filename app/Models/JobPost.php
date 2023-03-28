9c65b5da1c88c6ffcce16b1ef0c54d4573953520
Marcus Xavier <marcus.xavier@aluno.ufop.edu.br>
2023-03-28 20:37:55 -0300

    feat(JobPost.php): add JobPost model with fillable fields 'name', 'description', and 'job_contract_type_id'

---
+++ app/Models/JobPost.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class JobPost extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'job_contract_type_id',
+    ];
+}
