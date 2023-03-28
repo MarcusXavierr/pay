202c7d6dd3d1017b0d1bdb872a6a5a31ecdad066
Marcus Xavier <marcusxavierr123@gmail.com>
2023-03-28 20:52:40 -0300

    fix(migration): fix migration type name

--- database/migrations/2023_03_28_233150_create_job_contract_types_table.php
+++ database/migrations/2023_03_28_233150_create_job_contract_types_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('job_contract_types', function (Blueprint $table) {
             $table->id();
-            $table->name('name');
+            $table->string('name');
             $table->timestamps();
         });
     }
