e7b27ca939ced7f95ac5779710556bc7ab613b4b
Marcus Xavier <marcusxavierr123@gmail.com>
2023-03-29 19:32:24 -0300

    feat(DatabaseSeeder.php): add createAdmin method to create an admin user and assign admin role to it refactor(DatabaseSeeder.php): call RoleSeeder and CandidateSeeder in run method

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
+use App\Support\Constants\UserRole;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            RoleSeeder::class,
+            CandidateSeeder::class,
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->createAdmin();
+    }
+
+    private function createAdmin()
+    {
+        $user = User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+
+        $user->assignRole(UserRole::ADMIN);
     }
 }
