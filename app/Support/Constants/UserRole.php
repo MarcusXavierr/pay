6762e32e34662e7342b46351ddc43e24523242a1
Marcus Xavier <marcusxavierr123@gmail.com>
2023-03-28 22:53:30 -0300

    feat(UserRole.php): add allRoles method to return all user roles as a collection

--- app/Support/Constants/UserRole.php
+++ app/Support/Constants/UserRole.php
@@ -2,8 +2,18 @@
 
 namespace App\Support\Constants;
 
+use Illuminate\Database\Eloquent\Collection;
+
 class UserRole
 {
     public const ADMIN = 'admin';
     public const CANDIDATE = 'candidate';
+
+    public static function allRoles(): Collection
+    {
+        return collect([
+            self::ADMIN,
+            self::CANDIDATE,
+        ]);
+    }
 }
