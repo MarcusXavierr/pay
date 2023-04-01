4927302032be57b5b13caec850ce76a7aa20edf2
Marcus Xavier <marcusxavierr123@gmail.com>
2023-03-31 21:16:28 -0300

    Refactor job post controller to use JobPostRequest and ManageJobPostService for CRUD operations

--- app/Http/Controllers/JobPostController.php
+++ app/Http/Controllers/JobPostController.php
@@ -2,60 +2,69 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\JobPostRequest;
 use App\Models\JobContractType;
 use App\Models\JobPost;
-use Illuminate\Http\Request;
+use App\Services\ManageJobPostService;
+use Exception;
 
 class JobPostController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $jobPosts = JobPost::paginate(10);
         return view('job-posts.index', compact('jobPosts'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         $jobCategories = JobContractType::all();
         return view('job-posts.create', compact('jobCategories'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    public function store(JobPostRequest $request)
     {
-        //
+        try {
+            app(ManageJobPostService::class)->createJob($request->validated());
+
+            return redirect()->route('manage-job-posts.index')->with('success', 'Vaga de emprego criada com sucesso!');
+        } catch (Exception $e) {
+            report($e);
+
+            return redirect()->route('manage-job-posts.index')->with('error', 'Erro ao criar vaga de emprego');
+        }
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(JobPost $jobPost)
+    public function edit($id)
     {
+        $jobPost = JobPost::find($id);
         $jobCategories = JobContractType::all();
         return view('job-posts.edit', compact('jobPost', 'jobCategories'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, JobPost $jobPost)
+    public function update(JobPostRequest $request, $id)
     {
-        //
+        try {
+            app(ManageJobPostService::class)->updateJob($id, $request->validated());
+
+            return redirect()->route('manage-job-posts.index')->with('success', 'Vaga atualizada com sucesso');
+        } catch (Exception $e) {
+            report($e);
+
+            return redirect()->route('manage-job-posts.index')->with('error', 'Erro ao atualizar vaga');
+        }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(JobPost $jobPost)
+    public function destroy($id)
     {
-        //
+        try {
+            app(ManageJobPostService::class)->deleteJob($id);
+
+            return redirect()->route('manage-job-posts.index')->with('success', 'Vaga deletada com sucesso');
+        } catch (Exception $e) {
+            report($e);
+
+            return redirect()->route('manage-job-posts.index')->with('error', 'Erro ao deletar vaga');
+        }
     }
 }
