a7de28d0e9db140af90e28c77c9242cb3a7dae0d
Marcus Xavier <marcusxavierr123@gmail.com>
2023-03-31 21:16:09 -0300

    Refactor JobPost model to add category relationship with JobContractType in a BelongsTo relation

--- app/Models/JobPost.php
+++ app/Models/JobPost.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class JobPost extends Model
@@ -11,7 +12,7 @@ class JobPost extends Model
     use HasFactory;
 
     protected $fillable = [
-        'name',
+        'title',
         'description',
         'job_contract_type_id',
     ];
@@ -19,5 +20,9 @@ class JobPost extends Model
     public function users(): BelongsToMany
     {
         return $this->belongsToMany(User::class);
+    }
+
+    public function category(): BelongsTo {
+        return $this->belongsTo(JobContractType::class);
     }
 }
