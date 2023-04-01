69f2120fdcd53c61c0e3a80e57459e2f1101bbfb
Marcus Xavier <marcusxavierr123@gmail.com>
2023-03-31 21:16:17 -0300

    refactor(JobContractType.php): change BelongsToMany relation to HasMany relation in jobs() method

--- app/Models/JobContractType.php
+++ app/Models/JobContractType.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class JobContractType extends Model
 {
@@ -14,8 +14,8 @@ class JobContractType extends Model
         'name'
     ];
 
-    public function jobs(): BelongsToMany
+    public function jobs(): HasMany
     {
-        return $this->belongsToMany(JobPost::class);
+        return $this->hasMany(JobPost::class);
     }
 }
